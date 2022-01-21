b8319f270d3c10e2065cc5b2e4cb646b6a560aab
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-01-21 13:04:37 -0500

    Add an AUTH ME url

--- suba_safe_back/applications/users/urls.py
+++ suba_safe_back/applications/users/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,12 +13,15 @@ app_name = 'users_app'
 urlpatterns = [
     # Url que carga un HTML template para ingresar al registro con Google
     path('login/', views.LoginUser.as_view(), name='login'),
+
     # Url para la validación de un token Firebase y el registro del usuario
     path('api/google-login/', views.GoogleLoginAPIView.as_view(), name='google-login'),
-    # 
-    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # Url para registrar un usuario desde la aplicación interna
-    path('api/auth/register/', views.RegisterAPIView.as_view(), name='token_obtain_pair'),
+    path('api/auth/register/', views.RegisterAPIView.as_view(), name='auth-registrar'),
     # Url para receptar el token enviado por correo electrónico
-    path('verificar-email/', views.VerifyEmail.as_view(), name='verificar-email'),
+    path('api/verificar-email/', views.VerifyEmail.as_view(), name='verificar-email'),
+    # Url para iniciar sesión en una cuenta activada
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # Url para la obtención de los datos personales
+    path('api/auth/me/', views.UserAuthView.as_view(), name='auth-me'),
 ]
