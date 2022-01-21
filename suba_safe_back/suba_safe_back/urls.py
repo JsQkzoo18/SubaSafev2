f35a80227fd541baf72a9e0963c413fb4ef64f85
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-01-21 15:58:51 -0500

    Make posible to see media files

--- suba_safe_back/suba_safe_back/urls.py
+++ suba_safe_back/suba_safe_back/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# Imports de Django
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+# Imports de DRF
+from rest_framework import permissions
 
 # Imports de DRF-YASG
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -57,4 +63,5 @@ urlpatterns = [
     path('', include('applications.auction.routers')), 
     path('', include('applications.payment.routers')), 
     path('', include('applications.comment.routers')), 
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
